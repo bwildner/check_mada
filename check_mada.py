121b99e410092af5be3c3b87a0ee2c4e9b485b0f
bwildner <bwildner@yahoo.de>
2017-11-07 14:13:20 +0100

    nun mit Mada

--- check_mada.py
+++ check_mada.py
@@ -8,13 +8,16 @@
 # Suchen der NCSerienIBN der Maschine
 # Pruefen ob 17400 (SW Version) enthalten und Ausgabe der Daten 
 # Bei mehreren NCSerienIBN, werden alle ausgegeben 
+# Eingabe von 3 Maschinendaten moeglich
 
 import os
 import sys
 from PyQt4 import uic 
 from PyQt4.QtGui import QWidget, QMainWindow, QVBoxLayout, QTextEdit, QMessageBox, QApplication
 from test.test_pep277 import filenames
-from time import time, clock
+from time import clock, ctime
+import logging
+
 
 
  
@@ -22,13 +25,18 @@ form_class = uic.loadUiType("mnummer_gui.ui")[0]                 # Load the UI
 
 
 
-def search2(path, extension):
+logging.basicConfig(filename="check_mada.log",level = logging.DEBUG,format = "%(asctime)s [%(levelname)-8s] %(message)s")
+
+ 
+logging.info("****************Start Logging****************")
+
+
+def searchfiles(path, extension):
     """
         sucht alle Dateien in path mit der angegebenen extension
         extension with leading point, for example: ".png"
         Rueckgabe ist eine Liste der Dateien mit Pfadangabe
     """
-    print "search"
     for root, dirs, filenames in os.walk(path):
         for filename in filenames:
             if os.path.splitext(filename)[-1] == extension:
@@ -57,6 +65,22 @@ class MyWindowClass(QMainWindow, form_class):
     def eingabe_clicked(self):                  #  button event handler
         eingabe = str(self.mnummer_textfeld.text())
         
+        if self.checkBox17400.checkState() ==2:
+            search17400=True
+        else:
+            search17400=False
+            
+            
+        if len(self.mada1.text())>0:
+            searchMada=True
+        else:
+            searchMada=False
+            
+            
+        logging.debug( "17400: " + str(search17400))
+        logging.debug("Mada: " + str(searchMada))
+        
+        
         #pruefen der eingabe und Fehlermeldung wenn noetig
         if not eingabe.isdigit() or len(eingabe)<>5:
             QMessageBox.warning(self, "Eingabefehler","Bitte die MNummer 5stellig eingeben.",QMessageBox.Cancel, QMessageBox.NoButton,QMessageBox.NoButton)
@@ -74,56 +98,103 @@ class MyWindowClass(QMainWindow, form_class):
         #bei heller ordner="//heller.biz/hnt/ControlArch-MNo/"
         
         verzeichnis = ordner+"M"+eingabe[0:2]+"xxx/ARCHIVE-M"+eingabe
-        print verzeichnis
+        logging.debug(str( verzeichnis))
         t1= clock()        
-        result = list(search2(verzeichnis, '.arc')) #Alle .arc Dateien im betreffenden Verz suchen
-        print "Dateien suchen: " , (clock()-t1)
+        result = list(searchfiles(verzeichnis, '.arc')) #Alle .arc Dateien im betreffenden Verz suchen
+        logging.info("Dateien suchen: " +str( (clock()-t1)))
+        laengedateiname1=0
+        laengedateiname2=0
         
         i = 0 #zaehler fuer dateien
         anzahl17400 =0 #zaehler fuer fenstergroesse
+        anzahlmada=0
         #Alle Dateien mit .arc nach 17400 durchsuchen
         t1= clock() 
         while i < len(result):
             with open(result[i],'r') as myfile:
                 
                 t2=clock()
-                #Kontrolle ob ;nccomp in den ersten 1000byte steht
+                #Kontrolle ob ;nckcomp in den ersten 1000byte steht
                 inhalt1 = myfile.read(1000)
-                print i, ". Datei einlesen zur Headerkontrolle", (clock()-t2)
+                logging.info(str(i)+str(myfile.name) + " Datei einlesen zur Headerkontrolle"+ str((clock()-t2)))
                 if inhalt1.find(";NCKComp",1,1000)>0: #Nckcomp vorhanden dann komplett einlesen und 17400 suchen
                     t3=clock()
                     inhalt = myfile.read()
-                    startpos = inhalt.find ('17400') #Position von 17400 im File ermitteln
-                    if startpos > 0:
-                        endpos = inhalt.find('17500', startpos) #Position von 17500 im File ermitteln
-                        #Text zwischen 17400 und 17500 im Ausgabefenster anzeigen
-                        ausgabefenster.edit.append(result[i])
-                        ausgabefenster.edit.append(inhalt[startpos-1:endpos-1])
-                        anzahl17400=anzahl17400+1 
-                        print i, " Datei komplett einlesen und durchsuchen: " ,(clock()-t3)," und bisherige Gesamtzeit: ", (clock()-t1)
+                    logging.info( str(i)+ " Nur Datei komplett einlesen ohne durchsuchen: " +str((clock()-t3)))
+                    if search17400 == True:
+                        startpos = inhalt.find ('17400') #Position von 17400 im File ermitteln
+                        if startpos > 0:
+                            endpos = inhalt.find('17500', startpos) #Position von 17500 im File ermitteln
+                            #Text zwischen 17400 und 17500 im Ausgabefenster anzeigen
+                            ausgabefenster.edit.append("Dateiname:\n "+ result[i] + "\n Datum: "+ ctime(os.path.getmtime(result[i])))
+                            
+                            if len(result[i])>laengedateiname1:
+                                laengedateiname1= len(result[i])
+                                                  
+                            ausgabefenster.edit.append(inhalt[startpos-1:endpos-1])
+                            
+                            anzahl17400=anzahl17400+1 
+                            logging.info(str(i)+" Datei komplett einlesen und durchsuchen: " +str((clock()-t3))+" und bisherige Gesamtzeit: "+str( (clock()-t1)))
+                    if searchMada == True:
+                        ausgabefenster2.edit.append("\n\nDateiname:\n "+ result[i] + "\n Datum: "+ ctime(os.path.getmtime(result[i])))
+                            
+                        filezeiger=0
+                        while inhalt.find (self.mada1.text(), filezeiger)>0:
+                            startpos = inhalt.find (self.mada1.text(), filezeiger) #Position von mada1 im File ermitteln
+                            if startpos > 0:
+                                endpos = inhalt.find('\n', startpos) #Zeilenende ermitteln
+                                #Text zwischen 17400 und 17500 im Ausgabefenster anzeigen
+                                
+                                ausgabefenster2.edit.append(inhalt[startpos-1:endpos])
+                                if len(result[i])>laengedateiname2:
+                                    laengedateiname2= len(result[i])
+                            
+                                anzahlmada=anzahlmada+1 
+                                filezeiger=endpos
+                                logging.info(str( i)+ " Datei komplett einlesen und durchsuchen: " +str((clock()-t3))+" und bisherige Gesamtzeit: "+str((clock()-t1)))
+                    
             i=i+1
-        self.close() #Dateien schliessen
+            myfile.close() #Dateien schliessen
         
         if len(result) ==0:
-            QMessageBox.warning(self, "Ergebnis","Keine Dateien mit 17400 gefunden", QMessageBox.Cancel, QMessageBox.NoButton, QMessageBox.NoButton)
+            QMessageBox.warning(self, "Ergebnis","Keine Dateien mit diesen Daten gefunden", QMessageBox.Cancel, QMessageBox.NoButton, QMessageBox.NoButton)
+        else:
+            if search17400==True:
+                ausgabefenster.setGeometry(100,100,laengedateiname1*6+100,anzahl17400*130+20)
+                ausgabefenster.show()
+                
+            if searchMada==True:
+                if filezeiger==0:
+                    QMessageBox.warning(self, "Ergebnis","Keine Dateien mit Madatum "+self.mada1.text()+ " gefunden", QMessageBox.Cancel, QMessageBox.NoButton, QMessageBox.NoButton)    
+                else:
+                    ausgabefenster2.setGeometry(100,100,laengedateiname2*6+100,anzahlmada*100)
+                    ausgabefenster2.show()
         
-        print i, " Dateien durchsuchen: " , (clock()-t1)
-        myWindow.destroy()
-        ausgabefenster.setGeometry(100,100,500,anzahl17400*100+20)
-        ausgabefenster.show()
+            
+        myWindow.close()
 
+        
+        logging.info(str( i)+ " Dateien durchsuchen: " +str( (clock()-t1)))
+        
+        
            
  
  
 app = QApplication(sys.argv)
 myWindow = MyWindowClass(None)
 ausgabefenster = AusgabefensterClass()
+ausgabefenster2 = AusgabefensterClass()
+
+#zwischenablage auslesen, pruefen und in das Textfeld kopieren
 zwischenablage = str(QApplication.clipboard().text())
 if len(zwischenablage) >5:
-    print ("Hallo")
-    
+    QApplication.clipboard().setText("")
 myWindow.mnummer_textfeld.setText(QApplication.clipboard().text())
+
+
 myWindow.show()
+
+
 app.exec_()
 
 """
